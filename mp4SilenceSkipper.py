0a106c5ca8a964b9b8ca25b179fffb917d4dbb86
Kai ISHIZUKA <kaiisiduka@gmail.com>
2021-03-20 20:06:46 +0900

    rename&english

--- mp4SilenceSkipper.py
+++ mp4SilenceSkipper.py
@@ -1,17 +1,18 @@
-#210320 mp4から無音部分を除去するプログラム/佐賀大19238032石塚快@firestarter2501
-#実行する前にFFmpegをインストールしてPATHを通すこと!!
-#noise,duration等を適宜変更して使用すること
+#210320 mp4SilenceSkipper/SagaUniv SE-Phys 19238032 Kai ISHIZUKA @firestarter2501
+#Install FFmpeg and pass the file path before running it!
+#To be used with appropriate changes to noise, duration, silencedetect, etc.
 
 import subprocess
 import os
 import glob
-#動画元素材の取得
+
+#Obtaining the original video material
 def mk_movieList(movie_folder):
     files = os.listdir(movie_folder)
     files = [x for x in files if x[-4:] == '.mp4']
     files = [x for x in files if x[0] != '.']
     return files
- 
+
 def mk_starts_ends(wk_dir,movie):
     os.chdir(wk_dir)
     output = subprocess.run(["ffmpeg","-i",movie,"-af", "silencedetect=noise=-30dB:duration=2","-f","null","-"], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
@@ -31,7 +32,7 @@ def mk_starts_ends(wk_dir,movie):
     print(time_list)
     starts_ends = list(zip(*[iter(time_list)]*2))
     return starts_ends
- 
+
 def mk_jumpcut(wk_dir,movie,starts_ends):
     os.chdir(wk_dir)
     for i in range(len(starts_ends)-1):
@@ -40,26 +41,26 @@ def mk_jumpcut(wk_dir,movie,starts_ends):
         print(splitfile)
         output = subprocess.run(["ffmpeg", "-i",movie,"-ss",str(starts_ends[i][1]),"-t",str(starts_ends[i+1][0]-starts_ends[i][1]),splitfile],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         print(output)
- 
-#動画の結合
+
+#Merge videos
 def join_movie(movie_files,out_path):
     videos = glob.glob(movie_files)
     print(videos)
  
-    # join対象のlist
+    #List of join targets
     with open("JumpCut/tmp.txt","w") as fp:
         lines = [f"file '{os.path.split(line)[1]}'" for line in videos]
-        #1,10,11,~のようになってしまうのを防止。並び替え
+        #Prevented from becoming 1,10,11,~, Sorted
         lineList = sorted(lines,key=len)
         fp.write("\n".join(lineList))
  
     output = subprocess.run(["ffmpeg","-f","concat","-i","JumpCut/tmp.txt","-c","copy",out_path],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     print(output)
- 
-#ディレクトリの指定
-print("元動画のディレクトリを絶対パスで指定してください ex. /Users/kishi/Downloads/")
+
+#Specifying a directory
+print("Specify the directory of the original video by absolute path. ex. /Users/kishi/Downloads/")
 movie_folder = input()
-print("動画を出力したいフォルダの絶対パス*.mp4を入力してください ex. /Users/kishi/Downloads/*.mp4")
+print("Enter the absolute path of the folder where you want to output the video *.mp4 ex. /Users/kishi/Downloads/*.mp4")
 movie_files = input()
 out_path = "join_out.mp4"
  
